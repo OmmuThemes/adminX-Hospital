75ea96ae849bcddb260ce03bed2cea42dd4ec97d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 19:12:01 +0700

    modify copyright

--- components/system/CMenu.php
+++ components/system/CMenu.php
@@ -11,7 +11,7 @@
  * 
  * @modify Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
- * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @copyright Copyright (c) 2017 Ommu Platform (www.ommu.co)
  * @link https://github.com/OmmuThemes/adminX-hospital
  *
  */
--- views/adminx/ui/front_collapse.php
+++ views/adminx/ui/front_collapse.php
@@ -4,7 +4,7 @@
  * @var $error array
  *
  * @author Putra Sudaryanto <putra@ommu.co>
- * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @copyright Copyright (c) 2017 Ommu Platform (www.ommu.co)
  * @link https://github.com/OmmuThemes/adminX-hospital
  * @contact (+62)856-299-4114
  *
